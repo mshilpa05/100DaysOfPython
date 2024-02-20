0dd79ce429b18deae9ddcec14e384d6265a5cfac
Shilpa Mathew <Shilpa.Mathew@serko.com>
2024-02-21 09:36:03 +1300

    Day1 BandNameGenerator

---
+++ BandNameGenerator/BandNameGenerator.py
@@ -0,0 +1,7 @@
+﻿def main():
+    street = input("Enter your street name\n")
+    petname = input("Enter your pet name\n")
+    print(f"Your band name is {street} {petname}")
+    
+if (__name__ == "__main__"):
+    main()
