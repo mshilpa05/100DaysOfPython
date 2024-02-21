5a95cd2f6dcac24f4ae0ec02ed6109c042c50e5e
Shilpa Mathew <Shilpa.Mathew@serko.com>
2024-02-21 14:07:10 +1300

    Day2 Tip Calculator

---
+++ TipCalculator/TipCalculator.py
@@ -0,0 +1,13 @@
+﻿def main():
+    print("Welcome to tip calculator\n")
+    totalbill = float(input("What was the total bill?\n"))
+    noofpeople = int(input("How many people to split the bill?\n"))
+    percentageoftip = int(input("What percentage tip would you like to give? 10, 12 or 15?\n"))
+
+    eachpersonpay = (totalbill + totalbill * percentageoftip / 100) / noofpeople
+
+    print(f"Each person should pay {eachpersonpay}")
+
+
+if __name__ == "__main__":
+    main()
